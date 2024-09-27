09081d2becd85c7d9ded4bcd65138b386028c11b
Emma Hedemann <ehedeman@c2r2s6.42wolfsburg.de>
2024-09-27 13:35:57 +0200

    ex01 done

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: ehedeman <ehedeman@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 11:39:27 by ehedeman          #+#    #+#             */
-/*   Updated: 2024/09/27 12:34:18 by ehedeman         ###   ########.fr       */
+/*   Updated: 2024/09/27 13:12:19 by ehedeman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ class Brain
 {
 private:
 
-	std::string ideas[100];
+	std::string 		ideas[100];
 	
 public:
 
-				Brain();
-				~Brain();
-				Brain(const Brain &copy);
-	Brain		&operator=(const Brain &copy);
+						Brain();
+						~Brain();
+						Brain(const Brain &copy);
+	Brain				&operator=(const Brain &copy);
 	
-	std::string	getIdea(int index);
-	void		setIdea(std::string idea, int index);
+	std::string	const	getIdea(int index);
+	void				setIdea(std::string idea, int index);
 	
-	void		printIdeas();
+	void				printIdeas();
 };
